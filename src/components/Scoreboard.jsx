4744078df685899334e04f1d0b1ff841723a36ce
Daniel Kantor <daniel.kantor2009@gmail.com>
2021-12-26 14:12:53 -0800

    Added downs, yards, and quarter

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './styles.css';
 
-class Scoreboard extends React.Component {
+class Scoreboard extends Component {
     state = {
         gameStatus: "scheduled",
         homeTeamName: "HOME",
@@ -9,26 +9,43 @@ class Scoreboard extends React.Component {
         timeRemainingMinutes: 13,
         timeRemainingSeconds: 54,
         homeScore: 0,
-        awayScore: 0
+        awayScore: 0,
+        downs: 0,
+        yardsToGo: 0,
+        quarter: 0
     };
-
-//#424549"
 
     render() { 
         return (
-            <div className="text-center" style={{backgroundColor: "#424549"}}>
-                <span className="font-family" style={{backgroundColor: "#23272A", padding: "25px", fontSize: "50px", color: "white"}}>{this.state.homeTeamName}</span>
+            <div className="text-center" style={{backgroundColor: "#222222"}}>
+                <span className="font-family" style={{backgroundColor: "#3B3B3B", padding: "25px", fontSize: "50px", color: "white"}}>{this.state.homeTeamName}</span>
                 <span className="font-family" style={{padding: "20px", fontSize: "60px", color: "white"}}>{this.state.timeRemainingMinutes}:{this.state.timeRemainingSeconds}</span>
-                <span className="font-family" style={{backgroundColor: "#23272A", padding: "25px", fontSize: "50px", color: "white"}}>{this.state.awayTeamName}</span>
+                <span className="font-family" style={{backgroundColor: "#3B3B3B", padding: "25px", fontSize: "50px", color: "white"}}>{this.state.awayTeamName}</span>
                 <p></p>
                 <span className="font-family" style={{padding: "10px", fontSize: "50px", marginRight: "285px", color: "white"}}>{this.state.homeScore}</span>
                 <span className="font-family" style={{padding: "10px", fontSize: "50px", color: "white"}}>{this.state.awayScore}</span>
                 <p></p>
-                <button className="btn-secondary m-2" style={{fontFamily: "Inconsolata"}} onClick={this.handleHomeIncrement}>Increment Home Score</button>
-                <button className="btn-secondary m-2" style={{fontFamily: "Inconsolata"}} onClick={this.handleAwayIncrement}>Increment Away Score</button>
+                <span className="font-family" style={{backgroundColor: "#3B3B3B", padding: "15px", fontSize: "35px", color: "white", marginLeft: "-10px", marginRight: "40px"}}>DOWN</span>
+                <span className="font-family" style={{backgroundColor: "#3B3B3B", padding: "15px", fontSize: "35px", color: "white", marginRight: "40px"}}>TO GO</span>
+                <span className="font-family" style={{backgroundColor: "#3B3B3B", padding: "15px", fontSize: "35px", color: "white"}}>QTR</span>
                 <p></p>
-                <button className="btn-secondary m-2" style={{fontFamily: "Inconsolata"}} onClick={this.handleHomeDecrement}>Decrement Home Score</button>
-                <button className="btn-secondary m-2" style={{fontFamily: "Inconsolata"}} onClick={this.handleAwayDecrement}>Decrement Away Score</button>
+                <span className="font-family" style={{padding: "10px", fontSize: "50px", color: "white"}}>{this.state.downs}</span>
+                <span className="font-family" style={{padding: "10px", fontSize: "50px", color: "white"}}>{this.state.yardsToGo}</span>
+                <span className="font-family" style={{padding: "10px", fontSize: "50px", color: "white"}}>{this.state.quarter}</span>
+                <p></p>
+                <button className="btn-secondary m-1" style={{fontFamily: "Inconsolata"}} onClick={this.handleHomeIncrement}>Increment Home Score</button>
+                <button className="btn-secondary m-1" style={{fontFamily: "Inconsolata"}} onClick={this.handleAwayIncrement}>Increment Away Score</button>
+                <p></p>
+                <button className="btn-secondary m-1" style={{fontFamily: "Inconsolata"}} onClick={this.handleHomeDecrement}>Decrement Home Score</button>
+                <button className="btn-secondary m-1" style={{fontFamily: "Inconsolata"}} onClick={this.handleAwayDecrement}>Decrement Away Score</button>
+                <p></p>
+                <button className="btn-secondary m-1" style={{fontFamily: "Inconsolata"}} onClick={this.handleDownsIncrement}>Increment Downs</button>
+                <button className="btn-secondary m-1" style={{fontFamily: "Inconsolata"}} onClick={this.handleYardsIncrement}>Increment Yards To Go</button>
+                <button className="btn-secondary m-1" style={{fontFamily: "Inconsolata"}} onClick={this.handleQuarterIncrement}>Increment Quarter</button>
+                <p></p>
+                <button className="btn-secondary m-1" style={{fontFamily: "Inconsolata"}} onClick={this.handleDownsDecrement}>Decrement Downs</button>
+                <button className="btn-secondary m-1" style={{fontFamily: "Inconsolata"}} onClick={this.handleYardsDecrement}>Decrement Yards To Go</button>
+                <button className="btn-secondary m-1" style={{fontFamily: "Inconsolata"}} onClick={this.handleQuarterDecrement}>Decrement Quarter</button>
             </div>
         );
     }
@@ -43,6 +60,21 @@ class Scoreboard extends React.Component {
         console.log("away score increased");
     }
 
+    handleDownsIncrement = () => {
+        this.setState({ downs: this.state.downs + 1});
+        console.log("downs increased");
+    }
+
+    handleYardsIncrement = () => {
+        this.setState({ yardsToGo: this.state.yardsToGo + 1});
+        console.log("yards to go increased");
+    }
+
+    handleQuarterIncrement = () => {
+        this.setState({ quarter: this.state.quarter + 1});
+        console.log("quarter increased");
+    }
+
     handleHomeDecrement = () => {
         if (this.state.homeScore !== 0) {
             this.setState({ homeScore: this.state.homeScore - 1});
@@ -50,7 +82,6 @@ class Scoreboard extends React.Component {
         } else {
             console.log("cannot decrease home score below zero");
         }
-        
     }
 
     handleAwayDecrement = () => {
@@ -59,6 +90,33 @@ class Scoreboard extends React.Component {
             console.log("away score decreased");
         } else {
             console.log("cannot decrease away score below zero");
+        }
+    }
+
+    handleDownsDecrement = () => {
+        if (this.state.downs !== 0) {
+            this.setState({ downs: this.state.downs - 1});
+            console.log("downs decreased");
+        } else {
+            console.log("cannot decrease downs below zero");
+        }
+    }
+
+    handleYardsDecrement = () => {
+        if (this.state.yardsToGo !== 0) {
+            this.setState({ yardsToGo: this.state.yardsToGo - 1});
+            console.log("yards decreased");
+        } else {
+            console.log("cannot decrease yards below zero");
+        }
+    }
+
+    handleQuarterDecrement = () => {
+        if (this.state.quarter !== 0) {
+            this.setState({ quarter: this.state.quarter - 1});
+            console.log("quarter decreased");
+        } else {
+            console.log("cannot decrease quarter below zero");
         }
     }
 }
